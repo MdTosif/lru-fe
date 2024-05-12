6f539519a60e73e8273b2dfdef70b97484da5ae7
mdtosif <md.tosif@zeeve.io>
2024-05-12 21:12:24 +0530

    improve cache list UI

--- src/apis/getAll.ts
+++ src/apis/getAll.ts
@@ -9,5 +9,10 @@ export default function useGetAllCacheApi() {
   return useSWR<{ key: string; value: string; timeout: string }[]>(
     `https://${import.meta.env.VITE_BE}/cache/all`,
     fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
   );
 }
--- src/components/Cache-List.tsx
+++ src/components/Cache-List.tsx
@@ -1,15 +1,57 @@
 import useWebSocket from "react-use-websocket";
 import List from "./List";
+import useGetAllCacheApi from "../apis/getAll";
+import { useEffect, useState } from "react";
+import IconRefresh from "./icon/refresh-icon";
 
 export default function CacheList() {
+  const [live, setLive] = useState("false");
+  const [refresh, setRefresh] = useState(0);
+  const [data, setData] = useState<
+    { key: string; value: string; timeout: string }[] | undefined
+  >([]);
   const {
-    lastJsonMessage: data,
+    lastJsonMessage: liveData,
   }: { lastJsonMessage: { key: string; value: string; timeout: string }[] } =
     useWebSocket(`wss://${import.meta.env.VITE_BE}/ws`);
+
+  const { data: apiData, mutate } = useGetAllCacheApi();
+
+  // for updating the data pointer to websocket data or api data on live/static button toggle
+  useEffect(() => {
+    setData(live === "true" ? liveData : apiData);
+  }, [apiData, live, liveData]);
+
+  // to refresh the cache list when using api
+  useEffect(() => {
+    console.log(refresh);
+    mutate();
+  }, [refresh]);
+
   return (
     <>
       <h2 className="card-title text-center m-auto">
-        Cache List (capacity - 40):
+        Cache List{" "}
+        {live !== "true" && (
+          <span
+            className="swap cursor-pointer"
+            onClick={() => setRefresh((e) => ++e)}
+          >
+            <IconRefresh />
+          </span>
+        )}
+        <label className="swap btn btn-outline btn-xs">
+          <input
+            type="checkbox"
+            value={live}
+            onChange={(e) =>
+              setLive(String(e.target.value === "true" ? false : true))
+            }
+          />
+          <span className="swap-on">Live</span>
+          <span className="swap-off">Static</span>
+          {/* {live} */}
+        </label>
       </h2>
       {data?.map &&
         data?.map((e) => (
@@ -18,7 +60,7 @@ export default function CacheList() {
             keyName={e.key}
             value={e.value}
             timestamp={e.timeout}
-          ></List>
+          />
         ))}
     </>
   );
